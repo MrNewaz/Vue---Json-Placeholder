d279350cad84b0bba8151da2e5a96a2d730bf708
Saif Rahman <saif.rahman@udplatforms.com>
2022-06-27 00:42:30 +0600

    Testing Vue

---
+++ src/components/Post.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="reminder task">
+    <h3>
+      {{ post.title }}
+    </h3>
+    <p>
+      {{ post.body }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Post",
+  props: {
+    post: Object,
+  },
+};
+</script>
+
+<style scope>
+.task {
+  background: #f4f4f4;
+  margin: 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+.task.reminder {
+  border-left: 5px solid green;
+}
+.task h3 {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
---
+++ src/components/Posts.vue
@@ -0,0 +1,29 @@
+<template>
+  <div v-for="post in posts" :key="post.id">
+    <Post :post="post" />
+  </div>
+</template>
+
+<script>
+import Post from "./Post.vue";
+export default {
+  name: "Posts",
+  components: {
+    Post,
+  },
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  mounted() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((json) => (this.posts = json))
+      .catch((error) => console.log(error));
+  },
+};
+</script>
+
+<style>
+</style>
